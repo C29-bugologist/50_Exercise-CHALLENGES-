2a2c4408c9dfc21626a0b988e072b6245c0c08b8
C29-bugologist <167443196+C29-bugologist@users.noreply.github.com>
2024-06-19 09:30:06 +0530

    Match Case

---
+++ Match case/positive or negetive.py
@@ -0,0 +1,10 @@
+Ninput = int(input("Enter the Number: "))
+if __name__ == "__main__":
+    match Ninput:
+        case 0:
+            print("Number is Zero")
+        case Ninput if Ninput > 0:
+            print("The number is positive")
+        case n if n < 0:
+            print("The number is negative")
+
